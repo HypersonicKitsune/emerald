d134573486cbbd7074ed79b3fb363018d8f2e100
HypersonicKitsune <hypersonic.kitsune@gmail.com>
2019-01-11 20:31:31 +0100

    Begin a new experiment

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+/***
+ * Current experiment based on the engine: an animated square
+ ***/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -9,21 +13,70 @@
 #include "./macros.h"
 #include "./geoitems.h"
 
-ALLEGRO_DISPLAY *display = NULL;
+// allegro's stuff
+ALLEGRO_DISPLAY     *display  = NULL;
+ALLEGRO_EVENT_QUEUE *ev_queue = NULL;
+ALLEGRO_TIMER       *timer    = NULL;
+
+Point pl; // player's position
+Vector vec; // current player's translation vector
+bool mainloop = true;
+bool redraw   = true;
 
 
 int main()
 {
 	init_allegro();
 	display = al_create_display(SCREEN_W, SCREEN_H);
+
+	al_install_keyboard();
+    ev_queue = al_create_event_queue();
+    timer = al_create_timer(1.0/25);
+    
+    al_register_event_source(ev_queue, al_get_display_event_source(display));
+    al_register_event_source(ev_queue, al_get_keyboard_event_source());
+    al_register_event_source(ev_queue, al_get_timer_event_source(timer));
+    
+    al_start_timer(timer);
+    
+	pl = set_point_coordinates(SCREEN_W/2, SCREEN_H/2);
+	vec = set_vec_coordinates(0, 0);
 	
-	Point pl;
-	pl = set_point_coordinates(200, 200);
+	// vec
+	float i = 0.0;
 	
-	Vector vec;
-	vec = set_vec_coordinates(64, 64);
-	
-    al_clear_to_color(GRAY);
+	while(mainloop)
+    {
+        // events
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(ev_queue, &ev);
+        
+        if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        {
+            mainloop = false;
+        }
+        else if(ev.type == ALLEGRO_EVENT_TIMER)
+        {
+            vec = set_vec_pol_coordinates(10, i+=0.1);
+            
+            point_translation(&pl, vec);
+
+            redraw = true;
+        }
+        
+        // graphics update
+        if(redraw && al_is_event_queue_empty(ev_queue))
+        {
+			al_clear_to_color(GRAY);
+			al_draw_filled_rectangle(pl.x, pl.y, pl.x+32, pl.y+32, BLUE);
+			al_flip_display();
+            
+            redraw = false;
+        }
+	}
+    
+    /**al_clear_to_color(GRAY);
+     * 
     al_draw_filled_rectangle(pl.x, pl.y, pl.x+32, pl.y+32, BLUE);
     al_flip_display();
     
@@ -37,6 +90,10 @@ int main()
 	
 	al_rest(5);
 	
+	al_destroy_display(display);**/
+	
+	al_destroy_timer(timer);
+	al_destroy_event_queue(ev_queue);
 	al_destroy_display(display);
 	
 	return EXIT_SUCCESS;
